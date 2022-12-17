3c0395c081b2f4f18843d8e0a893ca4c0e08fca6
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-12-17 09:29:59 -0800

    working on relaying objects values to the DOM

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 
- let myLibrary = [book1];
 
-function Book(title, author, status) {
+let myLibrary = [book1, book2, book3];
+ 
+
+function Book(title, author) {
     this.title = title
     this.author = author
-    this.status = status
+    
     
    }
    
-   const book1 = new Book('Shantaram', 'Gregory David Roberts', 'read')
+   const book1 = new Book('Shantaram', 'Gregory David Roberts')
+   const book2 = new Book('Les Miserables', 'Victor Hugo' )
+   const book3 = new Book('Death and the Dervish', 'Mesa Selimovic')
 
+
+   
    function addBooktoLib(){
     let title = prompt("Enter a book's title: ")
     let author = prompt("Enter author's name: ")
-    let status = prompt("Enhter reading status: ")
-       let book = new Book(title, author, status)
+    
+       let book = new Book(title, author)
        myLibrary.push(book);
        return myLibrary;
    }
-
- 
-   
 
    
