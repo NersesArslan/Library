66757789fb5fe23c06718fcfbcdc0886f1867804
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-12-19 18:05:41 -0800

    added delete button, figuring out how to add form on click

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
 
-
-
-
- 
-
 function Book(title, author) {
     this.title = title
     this.author = author
-    
-    
    }
    
-   const book1 = new Book('Shantaram', 'Gregory David Roberts')
-   const book2 = new Book('Les Miserables', 'Victor Hugo' )
-   const book3 = new Book('Death and the Dervish', 'Mesa Selimovic')
+   let myLibrary = [];
 
-   let myLibrary = [book1, book2, book3];
+   const shelf = document.querySelector('.shelf')
+   const btn = document.getElementById('btn')
 
-  ;
+   const loopThroughLibrary = (arr) => {   //function that loops through Library array
+    (arr.map(book => book.title + " - " + book.author)).forEach(element => {
+      console.log(element)
+      const book = document.createElement('div')
+      const text = document.createElement('p')
+      text.classList.add('title')
+        book.classList.add('book')
+        book.appendChild(text)
+        text.textContent = element
+        shelf.appendChild(book)
 
-  const addButton = document.getElementById('btn');
-  const shelf = document.querySelector('.shelf')
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('id', 'delete-btn')
+        deleteBtn.innerText = 'x'
+        deleteBtn.addEventListener('click', function(e) {
+          book.parentNode.removeChild(book)
+        })
+        book.appendChild(deleteBtn)       
+    }); 
+  }
+
+   btn.addEventListener('click', (e) => {
+      let title = prompt('Enter your book title: ')
+      let author = prompt('Enter author\'s name: ')
+      const book = new Book(title, author)
+      myLibrary.push(book)
+      loopThroughLibrary(myLibrary)
+      myLibrary = []
+    })
   
-
-  addButton.addEventListener('click', () => {
-    const book = document.createElement('div')
-  book.classList.add('book')
-    shelf.appendChild(book);
-    
-  })
+   
+ 
