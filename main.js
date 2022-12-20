277015af6fc3cbf1c9fd370c6e773c2d7dc5ce9f
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2022-12-20 13:29:35 -0800

    added form and basic functionality. Added read status

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
-function Book(title, author) {
-    this.title = title
-    this.author = author
-   }
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
    
    let myLibrary = [];
 
@@ -11,12 +13,24 @@ function Book(title, author) {
 
    const loopThroughLibrary = (arr) => {   //function that loops through Library array
     (arr.map(book => book.title + " - " + book.author)).forEach(element => {
-      console.log(element)
+      
       const book = document.createElement('div')
       const text = document.createElement('p')
+      const status = document.createElement('p')
+      let checkBox = document.querySelector('input[type=checkbox]') //checkbox 
+    
+    const readStatus = () => {if(checkBox.checked === true){
+      return 'Read'
+    } else {
+      return 'Not read'
+    }
+  }
+   status.textContent = readStatus()
+
       text.classList.add('title')
         book.classList.add('book')
         book.appendChild(text)
+        book.appendChild(status)
         text.textContent = element
         shelf.appendChild(book)
 
@@ -31,13 +45,23 @@ function Book(title, author) {
   }
 
    btn.addEventListener('click', (e) => {
-      let title = prompt('Enter your book title: ')
-      let author = prompt('Enter author\'s name: ')
-      const book = new Book(title, author)
-      myLibrary.push(book)
-      loopThroughLibrary(myLibrary)
-      myLibrary = []
+      document.getElementById('formElement').style.display ='flex'
+        
     })
   
-   
+   const submitBtn = document.getElementById('submitBtn')
+
+   submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const bookTitle = document.getElementById('bookTitle')
+    const bookAuthor = document.getElementById('bookAuthor')
+    const book = new Book(bookTitle.value, bookAuthor.value)
+    myLibrary.push(book)
+    loopThroughLibrary(myLibrary)
+    myLibrary = []
+    bookTitle.value = ''
+    bookAuthor.value = ''
+
+    document.getElementById('formElement').style.display ='none'
+   })
  
