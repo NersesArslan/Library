3bf408a11eaf6d661c4bf67f2f25bc8baf427c96
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2023-01-31 21:07:00 -0800

    did basic form validation

--- main.js
+++ main.js
@@ -1,67 +1,68 @@
-
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
-   
-   let myLibrary = [];
 
-   const shelf = document.querySelector('.shelf')
-   const btn = document.getElementById('btn')
+let myLibrary = [];
 
-   const loopThroughLibrary = (arr) => {   //function that loops through Library array
-    (arr.map(book => book.title + " - " + book.author)).forEach(element => {
-      
-      const book = document.createElement('div')
-      const text = document.createElement('p')
-      const status = document.createElement('p')
-      let checkBox = document.querySelector('input[type=checkbox]') //checkbox 
-    
-    const readStatus = () => {if(checkBox.checked === true){
-      return 'Read'
-    } else {
-      return 'Not read'
-    }
-  }
-   status.textContent = readStatus()
+const shelf = document.querySelector(".shelf");
+const btn = document.getElementById("btn");
+const form = document.querySelector('[data-form-element]')
 
-      text.classList.add('title')
-        book.classList.add('book')
-        book.appendChild(text)
-        book.appendChild(status)
-        text.textContent = element
-        shelf.appendChild(book)
+const loopThroughLibrary = (arr) => {
+  //function that loops through Library array
+  arr
+    .map((book) => book.title + " - " + book.author)
+    .forEach((element) => {
+      const book = document.createElement("div");
+      const text = document.createElement("p");
+      const status = document.createElement("p");
+      let checkBox = document.querySelector("input[type=checkbox]"); //checkbox
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.setAttribute('id', 'delete-btn')
-        deleteBtn.innerText = 'x'
-        deleteBtn.addEventListener('click', function(e) {
-          book.parentNode.removeChild(book)
-        })
-        book.appendChild(deleteBtn)       
-    }); 
-  }
+      const readStatus = () => {
+        if (checkBox.checked === true) {
+          return "Read";
+        } else {
+          return "Not read";
+        }
+      };
+      status.textContent = readStatus();
 
-   btn.addEventListener('click', (e) => {
-      document.getElementById('formElement').style.display ='flex'
-        
-    })
-  
-   const submitBtn = document.getElementById('submitBtn')
+      text.classList.add("title");
+      book.classList.add("book");
+      book.appendChild(text);
+      book.appendChild(status);
+      text.textContent = element;
+      shelf.appendChild(book);
 
-   submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const bookTitle = document.getElementById('bookTitle')
-    const bookAuthor = document.getElementById('bookAuthor')
-    const book = new Book(bookTitle.value, bookAuthor.value)
-    myLibrary.push(book)
-    loopThroughLibrary(myLibrary)
-    myLibrary = []
-    bookTitle.value = ''
-    bookAuthor.value = ''
+      const deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("id", "delete-btn");
+      deleteBtn.innerText = "x";
+      deleteBtn.addEventListener("click", function (e) {
+        book.parentNode.removeChild(book);
+      });
+      book.appendChild(deleteBtn);
+    });
+};
 
-    document.getElementById('formElement').style.display ='none'
-   })
- 
+btn.addEventListener("click", (e) => {
+  form.style.display = "flex";
+});
+
+const submitBtn = document.getElementById("submitBtn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const bookTitle = document.getElementById("bookTitle");
+  const bookAuthor = document.getElementById("bookAuthor");
+  const book = new Book(bookTitle.value, bookAuthor.value);
+  myLibrary.push(book);
+  loopThroughLibrary(myLibrary);
+  myLibrary = [];
+  bookTitle.value = "";
+  bookAuthor.value = "";
+
+  document.getElementById("formElement").style.display = "none";
+});
